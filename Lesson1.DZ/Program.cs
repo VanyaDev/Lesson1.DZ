b003eba37a4ed4e37cd8b6c9e6d1cb0f061e20d7
gamingtv20181@gmail.com <gamingtv20181@gmail.com>
2020-06-14 10:53:35 +0300

    Добавьте файлы проекта.

---
+++ Lesson1.DZ/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lesson1.DZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ФИО:");
+            string input = Console.ReadLine();
+            Console.WriteLine("Введите возраст:");
+            string inputA = Console.ReadLine();
+            float A = float.Parse(inputA.Replace(".", ","));
+            Console.WriteLine("Введите рост:");
+            string inputB = Console.ReadLine();
+            float B = float.Parse(inputB.Replace(".", ","));
+            Console.WriteLine("Введите номер телефона:");
+            string inputC = Console.ReadLine();
+            Console.WriteLine("Введите ваш адрес:");
+            string inputF = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Ваше ФИО: " + input);
+            Console.WriteLine("Ваш возраст: " + inputA);
+            Console.WriteLine("Ваш рост: " + inputB);
+            Console.WriteLine("Ваш номер телефона: " + inputC);
+            Console.WriteLine("Ваш адресс: " + inputF);
+        }
+    }
+}
